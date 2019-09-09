2aa884071e88ccd9c168b78fc4a6deb26c76119c
RandomK3nt <55055835+RandomK3nt@users.noreply.github.com>
2019-09-08 20:11:36 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', (message) => {
+
+    if(message.content == 'k-help') {
+            message.reply('Sorry for the inconvinience but the bot is still being made so the help menu is not availible');
+    }
+});
+
+client.login('NjE5OTg5MTU5NzI5NDMwNTY5.XXWU6A.LEY6NPGaCQ8FE_OLVd5EYuDytyk');
